44a16896d4ea2aa080859d7a99e85d7f0acb105a
Jorge Crespo <jcrespo@jcrespoPC>
2017-04-03 17:18:12 -0300

    Doxygen doc

--- src/ledDriver.c
+++ src/ledDriver.c
@@ -1,7 +1,6 @@
 #include "ledDriver.h"
-
-
-/*
+  
+/**
  * Crea el driver, seteando los led apagados por inicializacion 
  *
  */
@@ -9,9 +8,11 @@ void ledDriver_create(){
 	driverAddress = 0;
 }
 
-/*
- * Enciende el led indicado por el parametro led 
+/**
+ * Enciende el led indicado por el parametro led. Si el parametro esta fuera del rango 0-15 no hace nada 
  *
+ * @param uint8_t led Indica el led a apagar, de 0 a 15.
+ * @return void
  */
 void ledDriver_led_on(uint8_t led){
 	if(led<16){
@@ -19,9 +20,11 @@ void ledDriver_led_on(uint8_t led){
 	}
 }
 
-/*
- * Apaga el led indicado por el parametro led 
+/**
+ * Apaga el led indicado por el parametro led. Si el parametro esta fuera del rango 0-15 no hace nada 
  *
+ * @param uint8_t led Indica el led a apagar, de 0 a 15.
+ * @return void
  */
 void ledDriver_led_off(uint8_t led){
 	if(led<16){
@@ -29,17 +32,24 @@ void ledDriver_led_off(uint8_t led){
 	}
 }
 
-/*
- * Enciende/Apaga varios leds segun la mascara recibida como parametro 
+
+/**
+ * Enciende/Apaga varios leds segun la mascara recibida como parametro  
  *
+ * @param uint16_t ledMask Indica los leds encendidos (1) y apagados (0) en codificacion binaria, 
+ * 			   uno por bit empezando por el led 0 en el LSB.
+ * @return void 
  */
 void ledDriver_multiple_led_set(uint16_t ledMask){
 	driverAddress = (uint16_t)ledMask;
 }
 
-/*
- * Devuelve el estado del led indicado por parametro 
+
+/**
+ * Devuelve el estado del led indicado por parametro. Si el parametro esta fuera del rango 0-15 devuelve el valor de error 0xFF 
  *
+ * @param uint8_t led Indica el led a consultar.
+ * @return uint8_t Indica el valor del led: encendido (1), apagado (0) o led inexistente (0xFF)
  */
 uint8_t ledDriver_get_state(uint8_t led){
 	uint8_t state=0xFF;
@@ -53,10 +63,12 @@ uint8_t ledDriver_get_state(uint8_t led){
 	return state;
 }
 
-/*
- * Enciende o apaga todos los led en funcion del parametro power
+
+/**
+ * Enciende o apaga todos los led en funcion del parametro power 
  *
- * power>0 Leds encendidos, power=0 Leds apagados
+ * @param uint8_t power Indica la accion a realizar:power>0 Leds encendidos, power=0 Leds apagados
+ * @return void
  */
 void ledDriver_all_led(uint8_t power){
 	if(power>0){
